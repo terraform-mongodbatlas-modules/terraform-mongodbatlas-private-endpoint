116e401fb15030482588cff6bc0f0d9713ad1fb0
rubenVB01 <95967197+rubenVB01@users.noreply.github.com>
2024-08-07 15:23:18 +0200

    feat: submodule implementation (#1)
    * module implementation
    
    * add mongo provider version constraint
    
    * correct file format
    
    * delete version constraint in mongo provider
    
    * correct file format
    
    * region from data source
    
    * correct file format
    
    * fix pr comments
    
    * Update modules/aws-private-link-cluster/variables.tf
    
    Co-authored-by: kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>
    
    * Update modules/aws-private-link-cluster/variables.tf
    
    Co-authored-by: kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>
    
    * remove version constraint from aws
    
    ---------
    
    Co-authored-by: kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>

---
+++ modules/aws-private-link-cluster/variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "The project id (e.g., 65def6ce0f722a1507105bb5)."
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC for the endpoint to use."
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "The ID of one or more subnets to create a network interface for the endpoint."
+  type        = list(string)
+}
+
+variable "security_group_ids" {
+  description = "The ID of one or more security groups to associate with the network interface. If no security groups are specified, the VPC's default security group is associated with the endpoint."
+  type        = list(string)
+  default     = null
+  nullable    = true
+}
