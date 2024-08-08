856d67d476b1403d6205d73d2a2969f2a1ee1149
rubenVB01 <rubenvibax@hotmail.com>
2024-08-08 14:36:32 +0200

    delete default project ids

--- examples/aws-private-link-cluster-geosharded/variables.tf
+++ examples/aws-private-link-cluster-geosharded/variables.tf
@@ -1,5 +1,4 @@
 variable "project_id" {
   description = "Atlas project id."
   type        = string
-  default     = "66b497f4c067bd1e20a8d36f"
 }
--- examples/aws-private-link-cluster-single-region/variables.tf
+++ examples/aws-private-link-cluster-single-region/variables.tf
@@ -1,5 +1,4 @@
 variable "project_id" {
   description = "Atlas project id."
   type        = string
-  default     = "66b48bcfc067bd1e20a8a302"
 }
